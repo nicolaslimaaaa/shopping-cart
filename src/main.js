e710357dc1f5feade9a75f2d1f5d96e528f45aa2
Nicolas Lima <nicolaslima1500@gmail.com>
2023-03-22 19:08:41 -0300

    Requisito 03 finalizado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement, createCustomElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-await fetchProductsList('computador');
-fetchProductsList();
+const addCreateProductElement = async () => {
+  const sectionContainer = document.querySelector('.container');
+
+  const array = await fetchProductsList('computador');
+
+  array.forEach((element) => {
+    const produto = createProductElement({
+      id: element.id,
+      title: element.title,
+      thumbnail: element.thumbnail,
+      price: element.price,
+    });
+
+    sectionContainer.appendChild(produto);
+  });
+};
+
+await addCreateProductElement();
